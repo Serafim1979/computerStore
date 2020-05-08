be06b6c35ec830d5f0c848166de420b322fa8ea1
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-08 20:43:19 +0400

    Started developing the router class

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \myStore\App();
 
-throw new Exception('Pages not found', 404);
