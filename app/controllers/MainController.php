5976fdbc5407ccae0409a99ea7010c21e2712e17
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-10 21:29:35 +0400

    Created the output of brands to the main page

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,6 +10,8 @@ use myStore\Cache;
 class MainController extends AppController
 {
     public function indexAction(){
+        $brands = \R::find('brand', 'LIMIT 3');
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands'));
     }
 }
