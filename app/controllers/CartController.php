020ff2b7b8331c17cf847d90e9a104907dac41ab
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-16 01:12:28 +0400

    Implemented adding products to the cart

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -2,7 +2,7 @@
 
 
 namespace app\controllers;
-
+use app\models\Cart;
 
 class CartController extends AppController
 {
@@ -20,6 +20,11 @@ class CartController extends AppController
                 $mod = \R::findOne('modification', 'id = ? AND product_id = ?', [$mod_id, $id]);
             }
         }
-        die;
+        $cart = new Cart();
+        $cart->addToCart($product, $qty, $mod);
+        if($this->isAjax()){
+            $this->loadView('cart_modal');
+        }
+        redirect();
     }
 }
