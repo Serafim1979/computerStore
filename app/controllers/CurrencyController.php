dfffedb5b02300fa73e9302000433e2a68d5a98d
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-17 22:34:32 +0400

    Finished shopping cart

--- app/controllers/CurrencyController.php
+++ app/controllers/CurrencyController.php
@@ -2,7 +2,7 @@
 
 
 namespace app\controllers;
-
+use app\models\Cart;
 
 class CurrencyController extends AppController
 {
@@ -12,6 +12,7 @@ class CurrencyController extends AppController
             $curr = \R::findOne('currency', 'code = ?', [$currency]);
             if(!empty($curr)){
                 setcookie('currency', $currency, time() + 3600*24*7, '/');
+                Cart::recalc($curr);
             }
         }
         redirect();
