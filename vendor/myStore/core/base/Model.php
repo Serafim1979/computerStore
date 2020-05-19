4c26adb90790f35d6215a4c654a2b6cab055b847
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-19 23:14:48 +0400

    Started developing the user authorization functionality.

--- vendor/myStore/core/base/Model.php
+++ vendor/myStore/core/base/Model.php
@@ -13,4 +13,12 @@ abstract class Model
     public function __construct(){
         Db::instance();
     }
+
+    public function load($data){
+        foreach($this->attributes as $name => $value){
+            if(isset($data[$name])){
+                $this->attributes[$name] = $data[$name];
+            }
+        }
+    }
 }
