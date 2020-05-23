e745345a99528e76afeb329f284bc6f03afa17b7
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-23 22:31:14 +0400

    implemented registration on the site

--- app/views/layouts/hardware.php
+++ app/views/layouts/hardware.php
@@ -218,6 +218,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 <script src="js/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/validator.js"></script>
 <script src="js/typeahead.bundle.js"></script>
 <!--<script src="js/simpleCart.min.js"> </script>-->
 
