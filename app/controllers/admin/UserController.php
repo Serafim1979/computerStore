19e6c7a5e52407a1328b1e1c6283fac2c72fa4a9
Serafim1979 <korolkov.ivan@mail.ru>
2020-05-29 22:53:05 +0400

    Implemented category management from the admin part of the site

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -7,6 +7,25 @@ use app\models\User;
 
 class UserController extends AppController
 {
+    public function indexAction(){
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 3;
+        $count = \R::count('user');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+        $users = \R::findAll('user', "LIMIT $start, $perpage");
+        $this->setMeta('Список пользователей');
+        $this->set(compact('users', 'pagination', 'count'));
+    }
+
+    public function editAction()
+    {
+        $user_id = $this->getRequestID();
+        $user = \R::load('user', $user_id);
+        $this->setMeta('Редактирование профиля пользователя');
+        $this->set(compact('user'));
+    }
+
     public function loginAdminAction(){
         if(!empty($_POST)){
             $user = new User();
